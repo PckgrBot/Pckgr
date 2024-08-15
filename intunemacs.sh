5b3ce218d608101061f6c30604e71c59b2bd1a5d
PckgrBot <119904223+PckgrBot@users.noreply.github.com>
2024-08-15 20:38:05 +1000

    Update intunemacs.sh

--- intunemacs.sh
+++ intunemacs.sh
@@ -321,9 +321,9 @@ checkCmdOutput () {
     local checkOutput="$1"
     exitStatus="$( echo "${checkOutput}" | grep --binary-files=text -i "exit" | tail -1 | sed -E 's/.*exit code ([0-9]).*/\1/g' || true )"
     if [[ ${exitStatus} -eq 0 ]] ; then
-        echo "${appname} ${appversion} succesfully installed."
+        #echo "${appname} ${appversion} succesfully installed."
         selectedOutput="$( echo "${checkOutput}" | grep --binary-files=text -E ": (REQ|ERROR|WARN)" || true )"
-        #echo "$selectedOutput"
+        echo "$selectedOutput"
     else
         echo "ERROR installing ${item}. Exit code ${exitStatus}"
         echo "$checkOutput"
