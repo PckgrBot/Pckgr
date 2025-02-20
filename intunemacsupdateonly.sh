d837db6be490911382b0f8e0c1d72824bf422fc9
PckgrBot <119904223+PckgrBot@users.noreply.github.com>
2025-02-20 11:22:01 +1100

    Update intunemacsupdateonly.sh

--- intunemacsupdateonly.sh
+++ intunemacsupdateonly.sh
@@ -315,6 +315,7 @@ newappversion=""
 # Dialog icon and overlay icon
 icon=""
 overlayicon=""
+forceupdate=""
 
 # Only check if appPath is not empty
 if [ -n "${appPath}" ]; then
@@ -338,7 +339,11 @@ addToDock="0" # with dockutil after installation (0 if not)
 dialog_command_file="/var/tmp/dialog.log"
 dialogBinary="/usr/local/bin/dialog"
 dockutil="/usr/local/bin/dockutil"
-installomatorOptions="BLOCKING_PROCESS_ACTION=prompt_user DIALOG_CMD_FILE=${dialog_command_file}" # Separated by space
+if [ "$forceupdate" = "true" ]; then
+    installomatorOptions="BLOCKING_PROCESS_ACTION=ignore DIALOG_CMD_FILE=${dialog_command_file}"
+else
+    installomatorOptions="BLOCKING_PROCESS_ACTION=prompt_user DIALOG_CMD_FILE=${dialog_command_file}"
+fi
 scriptVersion="10.5"
 # PATH declaration
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin
