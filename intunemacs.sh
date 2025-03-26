0b1404d8ca7b904f26edfb55ecbb75083f11585a
PckgrBot <119904223+PckgrBot@users.noreply.github.com>
2025-03-26 16:32:56 +1100

    revert

--- intunemacs.sh
+++ intunemacs.sh
@@ -311,12 +311,11 @@ LOGO="microsoft"
 item=""
 run=""
 newappversion=""
-forceupdate=""
-enabledialog="true"
 # Dialog icon and overlay icon
 icon=""
 overlayicon=""
-
+#forceupdate
+forceupdate=""
 
 # dockutil variables
 addToDock="0" # with dockutil after installation (0 if not)
@@ -523,7 +522,7 @@ else
     #echo "LOGO: $LOGO"
     #echo "icon: ${icon}"
 
-# display first screen
+    # display first screen
     dialogCMD=("$dialogBinary"
            --title none
            --icon "$icon"
@@ -538,11 +537,16 @@ else
     if [[ -n "$overlayicon" ]]; then
         dialogCMD+=("--overlayicon" ${overlayicon})
     fi
-    
-    if [ "$enabledialog" = "true" ]; then
-        "${dialogCMD[@]}" &
-        sleep 0.1
-    fi
+
+    #echo "dialogCMD: ${dialogCMD[*]}"
+
+    "${dialogCMD[@]}" &
+
+    #echo "$(date +%F\ %T) : SwiftDialog started!"
+
+    # give everything a moment to catch up
+    sleep 0.1
+fi
 
 # Check if the item is 'microsoftcompanyportal' and set the specific arguments
 if [[ "$item" == "microsoftcompanyportal" ]]; then
